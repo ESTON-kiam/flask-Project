3a0c3c2a88b3e518f02813a79089d7f0ea31805a
ESTON-kiam <engestonbrandonkiama@gmail.com>
2025-02-24 13:03:57 +0300

    update

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for, request, redirect
 from pymongo import MongoClient
 
+Client = MongoClient('localhost', 27017)
+
 app = Flask(__name__)
 
 
@@ -9,8 +11,34 @@ class Demo:
         self.name = "demo"
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        email = request.form['email']
+        dob = request.form['dob']
+        gender = request.form['gender']
+        course = request.form['course']
+        address = request.form['address']
+
+        students.insert_one({
+            'firstName': firstName,
+            'lastName': lastName,
+            'email': email,
+            'dob': dob,
+            'gender': gender,
+            'course': course,
+            'address': address
+        })
+        return redirect(url_for('registrationsuccessful'))
+
+    all_students = students.find()
+    return render_template("registration.html", students=all_students)
+
+
+@app.route('/registrationsuccessful')
+def registrationsuccessful():
     return render_template("index.html")
 
 
@@ -20,9 +48,8 @@ def user(name):
     return render_template("user.html", content=name, x=42, demo=demo)
 
 
-Client = MongoClient('localhost', 27017)
 db = Client.SchoolManagement
-students = Client.students
+students = db.students
 
 if __name__ == '__main__':
     app.run(debug=True)
